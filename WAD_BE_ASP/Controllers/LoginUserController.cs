147e97e9119ae9b9041090d888b3e5ac5d8d4a02
Calin Ovidiu <calinovidiu15@yahoo.ro>
2023-10-03 23:49:59 +0300

    Add project files.

---
+++ WAD_BE_ASP/Controllers/LoginUserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WAD_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginUserController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public LoginUserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] string email, [FromQuery] string password)
+        {
+            var users = _context.LoginUser.ToList();
+
+            var filteredUsers = users.Where(user => user.Email == email && user.Password == password).ToList();
+
+            if (filteredUsers.Any())
+            {
+                return Ok(filteredUsers);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WAD_BE_ASP/LoginUserModel.cs
@@ -0,0 +1,6 @@
+﻿public class LoginUserModel
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
