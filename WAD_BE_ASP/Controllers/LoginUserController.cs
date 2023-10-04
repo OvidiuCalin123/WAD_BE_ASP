814661fab38b0afdcde9f08f7e06413dcf2a5600
Calin Ovidiu <calinovidiu15@yahoo.ro>
2023-10-04 21:52:10 +0300

    Added hashing for password

--- WAD_BE_ASP/Controllers/LoginUserController.cs
+++ WAD_BE_ASP/Controllers/LoginUserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace WAD_Backend.Controllers
 {
@@ -19,19 +20,71 @@ namespace WAD_Backend.Controllers
         [HttpGet]
         public IActionResult Get([FromQuery] string email, [FromQuery] string password)
         {
-            var users = _context.LoginUser.ToList();
+            
+            var user = _context.LoginUser.FirstOrDefault(u => u.Email == email);
 
-            var filteredUsers = users.Where(user => user.Email == email && user.Password == password).ToList();
-
-            if (filteredUsers.Any())
+            if (user != null)
             {
-                return Ok(filteredUsers);
+                try
+                {
+                    
+                    if (IsBase64String(user.Password))
+                    {
+                        
+                        byte[] storedSaltAndHash = Convert.FromBase64String(user.Password);
+                        byte[] storedSalt = new byte[16];
+                        byte[] storedHash = new byte[32];
+                        Array.Copy(storedSaltAndHash, 0, storedSalt, 0, 16);
+                        Array.Copy(storedSaltAndHash, 16, storedHash, 0, 32);
+
+                        
+                        int iterations = 10000; 
+                        using (var pbkdf2 = new Rfc2898DeriveBytes(password, storedSalt, iterations))
+                        {
+                            byte[] newHash = pbkdf2.GetBytes(32); 
+
+                            
+                            if (newHash.SequenceEqual(storedHash))
+                            {
+                                return Ok("Login successful.");
+                            }
+                            else
+                            {
+                                
+                                return NotFound("Invalid password.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        
+                        return BadRequest("Invalid password format in the database.");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    
+                    return BadRequest($"Error: Invalid Base64 format for stored password. {ex.Message}");
+                }
             }
-            else
+
+            
+            return NotFound("User not found.");
+        }
+
+        private bool IsBase64String(string base64)
+        {
+            try
             {
-                return NotFound();
+                Convert.FromBase64String(base64);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
             }
         }
+
 
 
         [HttpPost]
@@ -39,36 +92,39 @@ namespace WAD_Backend.Controllers
         {
             try
             {
-                var newUser = new LoginUserModel
+                // Generate a random salt
+                byte[] salt = new byte[16];
+                using (var rng = new RNGCryptoServiceProvider())
                 {
-                    Name = name,
-                    Email = email,
-                    Password = password
-                };
+                    rng.GetBytes(salt);
+                }
 
-                _context.LoginUser.Add(newUser);
-                _context.SaveChanges();
+                
+                int iterations = 10000;
+                using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations))
+                {
+                    byte[] hash = pbkdf2.GetBytes(32); 
+                    byte[] saltAndHash = new byte[48]; 
+                    Array.Copy(salt, 0, saltAndHash, 0, 16);
+                    Array.Copy(hash, 0, saltAndHash, 16, 32);
 
-                return Ok("User created successfully.");
+                    var newUser = new LoginUserModel
+                    {
+                        Name = name,
+                        Email = email,
+                        Password = Convert.ToBase64String(saltAndHash)
+                    };
+
+                    _context.LoginUser.Add(newUser);
+                    _context.SaveChanges();
+
+                    return Ok("User created successfully.");
+                }
             }
             catch (Exception ex)
             {
                 return BadRequest($"Error: {ex.Message}");
             }
-        }
-
-
-
-        // PUT api/<LoginController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<LoginController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
