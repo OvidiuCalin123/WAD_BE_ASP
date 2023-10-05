8f828ebf69edd545e643c40fbd926f4486ea2231
Calin Ovidiu <calinovidiu15@yahoo.ro>
2023-10-05 21:51:15 +0300

    Added session management through JWT

--- WAD_BE_ASP/Controllers/LoginUserController.cs
+++ WAD_BE_ASP/Controllers/LoginUserController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Security.Cryptography;
 
 namespace WAD_Backend.Controllers
@@ -7,69 +11,77 @@ namespace WAD_Backend.Controllers
     [ApiController]
     public class LoginUserController : ControllerBase
     {
-
         private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
 
-
-        public LoginUserController(ApplicationDbContext context)
+        public LoginUserController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
-
-        
 
         [HttpGet]
         public IActionResult Get([FromQuery] string email, [FromQuery] string password)
         {
-            
             var user = _context.LoginUser.FirstOrDefault(u => u.Email == email);
 
             if (user != null)
             {
                 try
                 {
-                    
                     if (IsBase64String(user.Password))
                     {
-                        
                         byte[] storedSaltAndHash = Convert.FromBase64String(user.Password);
                         byte[] storedSalt = new byte[16];
                         byte[] storedHash = new byte[32];
                         Array.Copy(storedSaltAndHash, 0, storedSalt, 0, 16);
                         Array.Copy(storedSaltAndHash, 16, storedHash, 0, 32);
 
-                        
-                        int iterations = 10000; 
+                        int iterations = 10000;
                         using (var pbkdf2 = new Rfc2898DeriveBytes(password, storedSalt, iterations))
                         {
-                            byte[] newHash = pbkdf2.GetBytes(32); 
+                            byte[] newHash = pbkdf2.GetBytes(32);
 
-                            
                             if (newHash.SequenceEqual(storedHash))
                             {
-                                return Ok(user.isAdmin);
+                                var claims = new[]
+                                {
+                                    new Claim(ClaimTypes.Name, email),
+                                };
+
+                                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+                                var tokenDescriptor = new SecurityTokenDescriptor
+                                {
+                                    Subject = new ClaimsIdentity(claims),
+                                    Expires = DateTime.UtcNow.AddHours(1),
+                                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+                                };
+
+                                var tokenHandler = new JwtSecurityTokenHandler();
+                                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                                var tokenString = tokenHandler.WriteToken(token);
+
+                                return Ok(new { Token = tokenString, isAdmin = user.isAdmin });
                             }
                             else
                             {
-                                
                                 return NotFound("Invalid password.");
                             }
                         }
                     }
                     else
                     {
-                        
                         return BadRequest("Invalid password format in the database.");
                     }
                 }
                 catch (FormatException ex)
                 {
-                    
                     return BadRequest($"Error: Invalid Base64 format for stored password. {ex.Message}");
                 }
             }
 
-            
             return NotFound("User not found.");
         }
 
@@ -86,6 +98,52 @@ namespace WAD_Backend.Controllers
             }
         }
 
+        [HttpGet("/api/CheckUserIfAdmin")]
+        public IActionResult GetIsAdmin()
+        {
+            
+            var token = Request.Headers["Authorization"].FirstOrDefault()?.Replace("Bearer ", "");
+
+            if (string.IsNullOrEmpty(token))
+            {
+                
+                return Unauthorized("No token provided.");
+            }
+
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]);
+                var validationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false, 
+                    ValidateAudience = false,
+                };
+
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out _);
+
+                
+                var email = principal.Identity.Name;
+
+                
+                var user = _context.LoginUser.FirstOrDefault(u => u.Email == email);
+
+                if (user != null)
+                {
+                    return Ok(user.isAdmin);
+                }
+                else
+                {
+                    return NotFound("User not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                return BadRequest($"JWT validation failed: {ex.Message}");
+            }
+        }
 
 
         [HttpPost]
@@ -127,5 +185,6 @@ namespace WAD_Backend.Controllers
                 return BadRequest($"Error: {ex.Message}");
             }
         }
+
     }
 }
